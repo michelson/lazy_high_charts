7033e012f588a7418736f807b036369f259e3fa8
deshi xiao <dxiao@redhat.com>
2010-09-14 09:19:51 +0800

    update spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,12 +7,7 @@ require 'action_view/helpers/javascript_helper'
 require 'action_view/helpers/text_helper'
 require 'action_view/helpers/tag_helper'
 
-include ActionView::Helpers::TagHelper::CaptureHelper
 
-module RSpecRails
-  class Application < ::Rails::Application
-  end
-end
 
 Dir["#{File.dirname(__FILE__)}/../lib/**/*.rb"].each {|f| require f}
 

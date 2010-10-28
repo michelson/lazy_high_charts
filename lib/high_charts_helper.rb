b063e598ba2334a1eb7cfa6ae5f732cb2d81ae11
Derek Lindahl <dlindahl@customink.com>
2010-10-27 21:01:30 -0400

    Fix double render issue when passing high_chart a block of additional options in Rails 3

--- lib/high_charts_helper.rb
+++ lib/high_charts_helper.rb
@@ -34,7 +34,7 @@ module HighChartsHelper
       });
     EOJS
     
-    return graph unless block_given?
+    return graph if Rails::VERSION::STRING =~ /^3/ or not block_given?
     concat graph 
   end
   

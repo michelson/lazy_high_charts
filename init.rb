394fec7ea42e553b2eacad2b9965b3cb882ae80b
Tommy xiao <xiaods@gmail.com>
2010-11-30 23:54:10 +0800

    update highchart generator

--- init.rb
+++ init.rb
@@ -1,10 +1,9 @@
 # coding: utf-8
-require 'high_chart'
-require "high_charts_helper"
+require 'LazyHighCharts'
+require 'LazyHighCharts/high_charts_helper'
 
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :high_charts => ["highcharts"]
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :ie_high_charts => ["excanvas.compiled.js"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :ie_high_charts => ["excanvas.compiled"]
 
-ActionView::Base.send(:include, HighChartsHelper)
-
+ActionView::Base.send :include, Formtastic::HighChartsHelper
 
--- lib/lazy_high_charts/high_charts_helper.rb
+++ lib/lazy_high_charts/high_charts_helper.rb
@@ -1,15 +1,17 @@
 # coding: utf-8
-module HighChartsHelper
- # ActiveSupport::JSON.unquote_hash_key_identifiers = false
-  def high_chart(placeholder, object  , &block)
-    object.html_options.merge!({:id=>placeholder})
-    object.options[:chart][:renderTo] = placeholder
-    high_graph(placeholder,object , &block).concat(content_tag("div","", object.html_options))
-  end
-  
 
-  def high_graph(placeholder, object, &block)
-    graph =<<-EOJS
+module LazyHighCharts
+  module HighChartsHelper
+    # ActiveSupport::JSON.unquote_hash_key_identifiers = false
+    def high_chart(placeholder, object  , &block)
+      object.html_options.merge!({:id=>placeholder})
+      object.options[:chart][:renderTo] = placeholder
+      high_graph(placeholder,object , &block).concat(content_tag("div","", object.html_options))
+    end
+
+
+    def high_graph(placeholder, object, &block)
+      graph =<<-EOJS
     <script type="text/javascript">
     jQuery(function() {
           // 1. Define JSON options
@@ -27,18 +29,18 @@ module HighChartsHelper
                         };
 
           // 2. Add callbacks (non-JSON compliant)
-          #{capture(&block) if block_given?}
+                                #{capture(&block) if block_given?}
           // 3. Build the chart
           var chart = new Highcharts.Chart(options);
       });
       </script>
     EOJS
-    if defined?(raw)
-      return raw(graph) 
-    else
-      return graph
+                                if defined?(raw)
+                                  return raw(graph) 
+                                else
+                                  return graph
+                                end
     end
   end
-  
 end
 

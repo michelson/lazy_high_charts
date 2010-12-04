e8218f21095afb09de4ef1a504840da1b1e25865
Tommy xiao <xiaods@gmail.com>
2010-12-04 22:59:53 +0800

    add generators support

---
+++ lib/generators/lazy_high_charts/install/install_generator.rb
@@ -0,0 +1,20 @@
+module LazyHighCharts
+  module Generators
+    class InstallGenerator < ::Rails::Generators:Base 
+      desc "This generator install highcharts javascripts"
+      @@version = "2.1.1"
+      
+      def install_highcharts
+        say_status("installing", "Highcharts javascript (github HEAD)", :green)
+        get "https://github.com/highslide-software/highcharts.com/raw/master/js/highcharts.src.js",,     "public/javascripts/highcharts.js"
+      rescue OpenURI::HTTPError
+        say_status("warning", "could not find Highcharts javascript file", :yellow)
+      end
+
+      def remove_highcharts
+        say_status("removing", "Highcharts javascript (github HEAD)", :green)
+        
+      end 
+    end
+  end
+end

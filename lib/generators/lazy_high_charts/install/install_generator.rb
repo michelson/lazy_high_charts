247d4f01e61877a5f6cf99e5e89ff2706f2fa8a1
Tommy xiao <xiaods@gmail.com>
2010-12-04 23:54:42 +0800

    fix minor generator bug

--- lib/generators/lazy_high_charts/install/install_generator.rb
+++ lib/generators/lazy_high_charts/install/install_generator.rb
@@ -5,7 +5,7 @@ module LazyHighCharts
       
       def install_highcharts
         say_status("installing", "Highcharts javascript (github HEAD)", :green)
-        get "https://github.com/highslide-software/highcharts.com/raw/master/js/highcharts.src.js",,     "public/javascripts/highcharts.js"
+        get "https://github.com/highslide-software/highcharts.com/raw/master/js/highcharts.src.js","public/javascripts/highcharts.js"
       rescue OpenURI::HTTPError
         say_status("warning", "could not find Highcharts javascript file", :yellow)
       end

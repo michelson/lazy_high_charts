04db79010440f3722d6e86f450598ca55bd39a04
Tommy xiao <xiaods@gmail.com>
2011-03-19 22:25:49 +0800

    tuning railtie

--- lib/lazy_high_charts.rb
+++ lib/lazy_high_charts.rb
@@ -10,6 +10,12 @@ module LazyHighCharts
       config.before_configuration do
         config.action_view.javascript_expansions[:high_charts] = %w(highcharts)
       end
+
+      initializer 'lazy_high_charts.initialize' do
+        ActiveSupport.on_load(:action_view) do
+          include LazyHighCharts::LayoutHelper
+        end
+      end
     end
   end
 

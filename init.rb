ce5ad867d2b939e001b6521d8ba9018839d29762
Tommy xiao <xiaods@gmail.com>
2011-03-19 21:48:41 +0800

    update init.rb

--- init.rb
+++ init.rb
@@ -4,6 +4,9 @@ require 'LazyHighCharts/layout_helper'
 
 ActionView::Base.send :include, LazyHighCharts::LayoutHelper
 
-module ActionView::Helpers::AssetTagHelper
-  register_javascript_expansion :high_charts => ["highcharts"]
+ActiveSupport.on_load(:action_view) do
+  ActiveSupport.on_load(:after_initialize) do
+    ActionView::Helpers::AssetTagHelper::register_javascript_expansion :high_charts => ["highcharts"]
+  end
 end
+

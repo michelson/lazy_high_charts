a8025a512444ae9145469b8c55c8a2facf081433
Frederico Araujo <fred.the.master@gmail.com>
2012-12-17 18:12:04 +0700

    removed jQuery dependency, rspec is passing now

--- lib/lazy_high_charts/layout_helper.rb
+++ lib/lazy_high_charts/layout_helper.rb
@@ -66,9 +66,11 @@ module LazyHighCharts
         graph =<<-EOJS
         <script type="text/javascript">
         (function() {
-          $(document).ready(function() {
+          var onload = window.onload;
+          window.onload = function(){
+            if (typeof onload == "function") onload();
             #{core_js}
-          });
+          };
         })()
         </script>
         EOJS

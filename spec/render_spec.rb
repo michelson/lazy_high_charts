cad98e6f7e268c2126225c1548c955d945263486
Jake Haber <jhaber@market76.com>
2013-06-27 09:55:44 -0400

    Syntax change

--- spec/render_spec.rb
+++ spec/render_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/spec_helper'
 require 'tempfile'
 
 describe 'LazyHighCharts::Render' do
-  if system "which phantomjs"
+  if system("which phantomjs").present?
     let(:chart) do
       LazyHighCharts::HighChart.new('graph') do |f|
         f.series(name: 'John', data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4])

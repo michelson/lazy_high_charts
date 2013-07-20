9d6b7f072a8e19d31e3f0a0e8e9ed2e6819fdaaf
Deshi Xiao <dxiao@redhat.com>
2013-07-20 08:31:33 +0800

    REV: removing  phantomjs feature to gem, it should be another service to handle.

--- lib/lazy_high_charts.rb
+++ lib/lazy_high_charts.rb
@@ -2,7 +2,6 @@ require File.join(File.dirname(__FILE__), *%w[lazy_high_charts core_ext string])
 require File.join(File.dirname(__FILE__), *%w[lazy_high_charts options_key_filter])
 require File.join(File.dirname(__FILE__), *%w[lazy_high_charts layout_helper])
 require File.join(File.dirname(__FILE__), *%w[lazy_high_charts high_chart])
-require File.join(File.dirname(__FILE__), *%w[lazy_high_charts render])
 if defined?(::Rails::Railtie)
   require File.join(File.dirname(__FILE__), *%w[lazy_high_charts railtie])
   require File.join(File.dirname(__FILE__), *%w[lazy_high_charts engine]) if ::Rails.version.to_s >= '3.1'

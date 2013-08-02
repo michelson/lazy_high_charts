635012cb3d41a7cbd840587360508f775093dd91
Peter Savichev (proton) <psavichev@gmail.com>
2013-08-02 18:39:27 +0400

    updated highstock to version 1.3.4 an highcharts to version 3.0.4

--- vendor/assets/javascripts/highcharts/adapters/mootools-adapter.js
+++ vendor/assets/javascripts/highcharts/adapters/mootools-adapter.js
@@ -1,5 +1,5 @@
 /*
- Highcharts JS v3.0.2 (2013-06-05)
+ Highcharts JS v3.0.4 (2013-08-02)
  MooTools adapter
 
  (c) 2010-2013 Torstein Hønsi
--- vendor/assets/javascripts/highcharts/stock/adapters/mootools-adapter.js
+++ vendor/assets/javascripts/highcharts/stock/adapters/mootools-adapter.js
@@ -1,5 +1,5 @@
 /*
- Highstock JS v1.3.2 (2013-06-05)
+ Highstock JS v1.3.4 (2013-08-02)
  MooTools adapter
 
  (c) 2010-2013 Torstein Hønsi

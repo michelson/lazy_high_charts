8f0e9e9eefbcb0c6ed692b915a1d8f1e5f6e23a1
Steven Clontz <steven.clontz@gmail.com>
2014-03-11 08:50:23 -0500

    Handle non-Rails environments

--- spec/lazy_high_charts_spec.rb
+++ spec/lazy_high_charts_spec.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe HighChartsHelper do
+describe "LazyHighCharts" do
   include LazyHighCharts::LayoutHelper
 
   before(:each) do

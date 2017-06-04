acc490518afe97e3e62ac03aac4cc526dad6522e
shekharrajak <shekharstudy@ymail.com>
2017-06-04 14:08:12 +0530

    minor change in tests

--- spec/iruby_notebook_spec.rb
+++ spec/iruby_notebook_spec.rb
@@ -1,16 +1,6 @@
 require File.dirname(__FILE__) + '/spec_helper'
-require 'iruby'
-require 'iruby'
 
 describe LazyHighCharts do
-
-  # Tell the name of new JS extension library to highcharts.js
-  context ".add_extension" do
-    it "should add extension name to lists" do
-      LazyHighCharts.add_extension("Hoge")
-      expect(LazyHighCharts.extension_lists.index("Hoge").nil?).to eq(false)
-    end
-  end
 
   # Add extension libraries loaded before LazyHighCharts.js
   context ".add_dependency" do
@@ -36,44 +26,4 @@ describe LazyHighCharts do
     end
   end
 
-
-  describe '.load_notebook' do
-    shared_examples 'inline script' do
-      it 'loads javascript sources as inline script' do
-        expect(IRuby).to receive(:display) do |rep|
-          expect(rep).to be_kind_of(IRuby::Display::Representation)
-          expect(rep.options[:mime]).to eq('application/javascript')
-
-          # highcharts.js
-          expect(rep.object).to match(/this\.highcharts=/)
-        end
-        expect(load_notebook).to eq(nil)
-      end
-    end
-
-    context 'without parameters' do
-      subject(:load_notebook) { LazyHighCharts.load_notebook }
-
-      include_examples 'inline script'
-    end
-
-    context 'given `assets` parameter is `:inline`' do
-      subject(:load_notebook) { LazyHighCharts.load_notebook(:inline) }
-
-      include_examples 'inline script'
-    end
-
-    context 'given `assets` parameter is `:cdn`' do
-      subject(:load_notebook) { LazyHighCharts.load_notebook(:cdn) }
-
-      it 'loads javascript sources from cdn' do
-        expect(IRuby).to receive(:display) do |rep|
-          expect(rep).to be_kind_of(IRuby::Display::Representation)
-          expect(rep.options[:mime]).to eq('application/javascript')
-          expect(rep.object).to include('http://code.highcharts.com/highcharts.js')
-        end
-        expect(load_notebook).to eq(nil)
-      end
-    end
-  end
 end
